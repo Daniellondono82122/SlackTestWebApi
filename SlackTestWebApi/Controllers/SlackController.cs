0830928267f97d47f04f3c535f02e1126736dab2
Daniel Londoño <dlondono@arrivelogistics.com>
2022-07-19 09:51:53 -0500

    create architecture methodAdd project files.

---
+++ SlackTestWebApi/Controllers/SlackController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SlackTestWebApi.Controllers
+{
+    public class SlackController : Controller
+    {
+        
+        public async Task<IActionResult> Test()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(400, ex.Message);
+            }
+        }
+    }
+}
