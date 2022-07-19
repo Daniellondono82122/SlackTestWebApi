a07b6bdd7b4a2bb1e619f5db33e965d73192c28b
Daniel Londoño <dlondono@arrivelogistics.com>
2022-07-19 09:51:53 -0500

    Add project files.

---
+++ SlackTestWebApi/Controllers/SlackController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SlackTestWebApi.Controllers
+{
+    public class SlackController : Controller
+    {
+        
+        public Task<IActionResult> Test()
+        {
+
+            //var url = $"{RecognizerApiUri}api/v1/Invoice/{id}/{action}";
+            //var headers = new Dictionary<string, string>();
+            //headers.Add("Authorization", token);
+            //var res = await HttpClientService.PostStringAsync(url, "", "application/json", headers);
+            //if (res.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            //{
+            //    var validateToken = await ManagementRepository.GetValidToken();
+            //    headers["Authorization"] = $"Bearer {validateToken}";
+            //    res = await HttpClientService.PostStringAsync(url, "", "application/json", headers);
+            //}
+            //var content = await res.Content.ReadAsStringAsync();
+            //var invoice = await GetInvoiceDetails(id, token, clientId);
+            //return (res.IsSuccessStatusCode, content ?? "Bad request - no content", invoice);
+
+        }
+    }
+}
